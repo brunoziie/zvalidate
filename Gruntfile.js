c6901a3c6fcb243fb6d510d975bf12fc2fe604bc
Bruno Silva <eu@brunoziie.com>
2013-07-15 11:56:34 -0300

    fix .gitignore file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,29 @@
-grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-        options: {},
-        build: {
-            src: 'src/zvalidate.js',
-            dest: 'dist/zvalidate.min.js'
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {},
+            build: {
+                src: 'src/zvalidate.js',
+                dest: 'dist/zvalidate.min.js'
+            }
+        },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'src/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'dist/',
+                ext: '.min.css'
+            }
         }
-    },
-    cssmin: {
-        minify: {
-            expand: true,
-            cwd: 'src/css/',
-            src: ['*.css', '!*.min.css'],
-            dest: 'dist/',
-            ext: '.min.css'
-        }
-    }
-});
+
+    });
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+};
